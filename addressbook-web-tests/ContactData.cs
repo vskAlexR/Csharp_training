4f6d13ae4306cbb410b44c4ff42cacd5eaa99777
vskAlexR <alexander.rassypin@gmail.com>
2022-04-11 18:08:39 +0300

    Задание №4

---
+++ addressbook-web-tests/ContactData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class ContactData
+    {
+        private string firstName;
+        private string lastName = "";
+        private string middleName = "";
+        private string mobileNumber = "";
+
+        public ContactData(string firstname)
+        {
+            this.firstName = firstname;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public string MobileNumber
+        {
+            get
+            {
+                return mobileNumber;
+            }
+            set
+            {
+                mobileNumber = value;
+            }
+        }
+    }
+}
